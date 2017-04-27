2136a42af268e026e8f71e96aaa1a142c5d71441
smeleyka <smeleyka@gmail.com>
2017-04-27 18:10:10 +0300

    second

--- src/MainClass.java
+++ src/MainClass.java
@@ -9,18 +9,19 @@ public class MainClass {
             stringToInt(test);
         } catch (MySizeArrayException e) {
             e.printStackTrace();
-        }catch (MyArrayDataException e1){
+        } catch (MyArrayDataException e1) {
             e1.printStackTrace();
         }
     }
-    public static void arrayCheck (String[][] arr) throws MySizeArrayException,MyArrayDataException{
+
+    public static void arrayCheck(String[][] arr) throws MySizeArrayException, MyArrayDataException {
         int arrSize = 4; //размер входящего массива
-        int arrSumm =0;
+
         for (int i = 0; i < arr.length; i++) {
-            if (arr.length!=arrSize) throw new MySizeArrayException();
-            else if (arr[i].length!=arrSize) {
+            if (arr.length != arrSize) throw new MySizeArrayException();
+            else if (arr[i].length != arrSize) {
                 int j = arr[i].length;
-                throw new MySizeArrayException("Неверный размер входящего массива (4x4): arr["+i+"]"+"["+j+"]");
+                throw new MySizeArrayException("Неверный размер входящего массива (4x4): arr[" + i + "]" + "[" + j + "]");
             }
         }
 //        int[][] intArr = new int [arr.length][arr.length];
@@ -36,17 +37,19 @@ public class MainClass {
 //        }
     }
 
-    public static void stringToInt (String[][]arr) throws MyArrayDataException{
-        int[][] intArr = new int [arr.length][arr.length];
+    public static void stringToInt(String[][] arr) throws MyArrayDataException {
+        int arrSumm = 0;
+        int[][] intArr = new int[arr.length][arr.length];
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 try {
                     intArr[i][j] = Integer.parseInt(arr[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Недопустимое значение в ячейке: [" + i + "]" + "[" + j + "]");
                 }
-                catch (NumberFormatException e){
-                    throw new MyArrayDataException("Недопустимое значение в ячейке: ["+i+"]"+"["+j+"]");
-                }
+                arrSumm+=intArr[i][j];
             }
         }
+        System.out.println(arrSumm);
     }
 }
